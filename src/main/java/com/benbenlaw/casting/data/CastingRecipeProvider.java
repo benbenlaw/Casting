0e01705484dfd6c14153373bdf689f0ffa94f507
benbenlaw <benlawrenceben@hotmail.co.uk>
2025-06-21 10:24:02 +0100

    2.0.29

--- src/main/java/com/benbenlaw/casting/data/CastingRecipeProvider.java
+++ src/main/java/com/benbenlaw/casting/data/CastingRecipeProvider.java
@@ -268,9 +268,9 @@ public class CastingRecipeProvider extends RecipeProvider {
 
 
         createSimpleSolidifierRecipe(getFluidStack("molten_quartz_enriched_iron", 90), CastingTags.Items.INGOT_MOLD,
-                ModdedTagBuilder.createNeoFabricItemTag("quartz_enriched_iron/ingot"), "quartz_enriched_iron/ingot", consumer);
+                ModdedTagBuilder.createNeoFabricItemTag("ingot/quartz_enriched_iron"), "quartz_enriched_iron/ingot", consumer);
         createSimpleSolidifierRecipe(getFluidStack("molten_quartz_enriched_iron", 810), CastingTags.Items.BLOCK_MOLD,
-                ModdedTagBuilder.createNeoFabricItemTag("quartz_enriched_iron/block"), "quartz_enriched_iron/block", consumer);
+                ModdedTagBuilder.createNeoFabricItemTag("block/quartz_enriched_iron"), "quartz_enriched_iron/block", consumer);
 
         createSimpleMixingRecipe(fluidList(
                         getFluidStack("molten_iron", 270), getFluidStack("molten_quartz", 250)),
